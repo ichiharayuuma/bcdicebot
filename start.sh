23120481cb2b1b22de513f86746bc138661124d9
ichiharayuuma <youma.20031209@gmail.com>
2024-08-27 17:13:54 +0900

    init

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+IGNORE_ERROR=
+# export BCDICE_PASSWORD=PleaseChangeMeIfYouUseThis
+# export BCDICE_DEFAULT_SYSTEM=DiceBot
+export BCDICE_MENTION_MODE=1
+# export BCDICE_RESULT_DISPLAY_FORMAT=V1
+# export BCDICE_SLASH_PREFIX=bcdice
+# export BCDICE_SLASH_SHORT_PREFIX=br
+# export BCDICE_STANDARD_INPUT_DISABLED=disabled
+# export BCDICE_SLASH_TABLE_SUGGESTION_DISABLED=disabled
+
+java -jar discord-bcdicebot.jar "$DISCORD_BOT_TOKEN" "$BCDICE_API_URL" "$IGNORE_ERROR"
